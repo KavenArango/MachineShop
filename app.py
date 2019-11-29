44f9edb6814bbfa50074bde08b775f31fbf6dd26
KavenArango <kavenarango12@hotmail.com>
2019-11-28 23:39:14 -0500

    changed white space

--- app.py
+++ app.py
@@ -4,14 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
 app.config['SECRET_KEY'] = 'KILLME'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://MachineShop:KAVENSTEVESHANNONALLDUMB@25.78.65.33/machineshop'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
 
 from apps.Hompage.routes import Main_View
 from apps.StudentPage.routes import Student_view
@@ -28,7 +26,7 @@ app.register_blueprint(Staff_View)
 app.register_blueprint(Machine_View)
 app.register_blueprint(Booking_View)
 from flask_nav import Nav
-from flask_nav.elements import Navbar,Subgroup,View,Link,Text,Separator
+from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
 
 bootstrap = Bootstrap(app)
 
@@ -37,23 +35,19 @@ nav = Nav(app)
 
 @nav.navigation('my_nav')
 def create_nav():
-    Machine_Des = View('Machine Descriptions' , 'Machine_View.Machine')
+    Machine_Des = View('Machine Descriptions', 'Machine_View.Machine')
     Home_view = View('Home', 'Main_View.home')
     Cnc_view = View('CNC Booking', 'Booking_View.CNC')
-    Bridgeport_view = View('BridgePort Booking' , 'Booking_View.Bridgeport')
+    Bridgeport_view = View('BridgePort Booking', 'Booking_View.Bridgeport')
     Lathe_view = View('Lathe Booking', 'Booking_View.Lathe')
-    Syil_view = View('Syil Booking','Booking_View.Syil')
+    Syil_view = View('Syil Booking', 'Booking_View.Syil')
     Booking_view = Subgroup('Booking',
                             Cnc_view,
                             Bridgeport_view,
                             Lathe_view,
                             Syil_view)
-    return Navbar('Machine Shop', Home_view,Machine_Des,Booking_view)
-
-
+    return Navbar('Machine Shop', Home_view, Machine_Des, Booking_view)
 
 
 if __name__ == '__main__':
     app.run()
-
-
--- apps/accounts/routes.py
+++ apps/accounts/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, flash, redirect, url_for,app
+from flask import Blueprint, render_template, flash, redirect, url_for, app
 from apps.accounts.forms import LoginForm, SignupForm
-from app import db,bcrypt
+from app import db, bcrypt
 from apps.accounts.models import Users
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -20,17 +20,15 @@ def login_form():
     return render_template(template, title=title, form=form)
 
 
-
 @login_view.route('/signup', methods=['Get', 'Post'])
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = Users(first_name=form.first_name.data, last_name=form.last_name.data,username=form.username.data,
+        user = Users(first_name=form.first_name.data, last_name=form.last_name.data, username=form.username.data,
                      email=form.email.data, password=hashed_password)
         session.add(user)
         session.commit()
         flash('Your account has been created! You may now log in', 'success')
         return redirect(url_for('/'))
-
     return render_template("accounts/signup.html", title="Signup", form=form)
