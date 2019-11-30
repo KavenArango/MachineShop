04cdb2494a3f1154492425b4946c0f26eb8bde28
Steven Portillo <steven.portillo@snhu.edu>
2019-11-30 16:03:51 -0500

    signup page functionality

--- apps/accounts/routes.py
+++ apps/accounts/routes.py
@@ -2,17 +2,15 @@ from flask import Blueprint, render_template, flash, redirect, url_for,app
 from apps.accounts.forms import LoginForm, SignupForm
 from app import db,bcrypt
 from apps.accounts.models import Users
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+pymysql://MachineShop:KAVENSTEVESHANNONALLDUMB@25.78.65.33/machineshop')
-Session = sessionmaker(engine)
-session = Session()
+
+
+
 
 login_view = Blueprint('login', __name__)
 
 
-@login_view.route('/', methods=['Get', 'Post'])
+@login_view.route('/', methods=['Get', 'POST'])
 def login_form():
     template = "accounts/login.html"
     title = "Login"
@@ -21,16 +19,18 @@ def login_form():
 
 
 
-@login_view.route('/signup', methods=['Get', 'Post'])
+@login_view.route('/signup', methods=['get', 'post'])
 def signup():
     form = SignupForm()
-    if form.validate_on_submit():
+    if form.is_submitted():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = Users(first_name=form.first_name.data, last_name=form.last_name.data,username=form.username.data,
+        user = Users(first_name=form.first_name.data, last_name=form.last_name.data, username=form.username.data,
                      email=form.email.data, password=hashed_password)
-        session.add(user)
-        session.commit()
+        db.session.add(user)
+        db.session.commit()
         flash('Your account has been created! You may now log in', 'success')
-        return redirect(url_for('/'))
+        return redirect(url_for('login.login_form'))
+
+
 
     return render_template("accounts/signup.html", title="Signup", form=form)
