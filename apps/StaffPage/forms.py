a5e2e6da811dabee6cc720d7d327f0f1e1d514d5
shannon-donahue <shannon.donahue@snhu.edu>
2019-12-04 14:40:06 -0500

    student search and detail view

---
+++ apps/StaffPage/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, length, Email, DataRequired,EqualTo,ValidationError
+from apps.accounts.models import Users
+
+class Staff_Student(Form):
+    first_name = StringField('first name')
+    last_name = StringField('last name')
+    email = StringField('Email')
+    major = StringField('Major')
+    machine = StringField('Machine')
+
