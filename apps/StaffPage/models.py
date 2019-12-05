b19aa1a77bd1101fde1d6ab3848abf3e9277080d
Steven Portillo <steven.portillo@snhu.edu>
2019-12-05 13:27:27 -0500

    migration

--- apps/StaffPage/models.py
+++ apps/StaffPage/models.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from app import db
 from apps.accounts import models
+from apps.StudentPage import models
+from apps.Machine import models
 class Staff(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-
+class Request(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    machine_id = db.Column(db.Integer, db.ForeignKey('machines.id'))
+    level_id = db.Column(db.Integer, db.ForeignKey('levels.id'))
