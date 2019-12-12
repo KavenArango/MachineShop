af682dd7df11492ff7b5db76b1173f89c6a5679c
shannon-donahue <shannon.donahue@snhu.edu>
2019-12-11 22:29:06 -0500

    idk

--- apps/StaffPage/forms.py
+++ apps/StaffPage/forms.py
@@ -2,12 +2,20 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, length, Email, DataRequired,EqualTo,ValidationError
 from apps.accounts.models import Users
+from wtforms_components import read_only
 
 class Staff_Student(Form):
     first_name = StringField('first name')
     last_name = StringField('last name')
     email = StringField('Email')
     major = StringField('Major')
+
+    def __init__(self, *args, **kwargs):
+        super(Staff_Student, self).__init__(*args, **kwargs)
+        read_only(self.first_name)
+        read_only(self.last_name)
+        read_only(self.email)
+        read_only(self.major)
 
 class Staff_Request(Form):
     first_name = StringField('first name')
