193c9e825e55150e4265201bbe2befdf640e526e
KavenArango <kavenarango12@hotmail.com>
2019-12-15 20:48:46 -0500

    ajax set

--- apps/BookingPage/routes.py
+++ apps/BookingPage/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request
 from flask import Flask
 from app import db
 from apps.accounts.models import Users
@@ -27,46 +27,45 @@ def Machine_Details(machine_id):
     title = "Reserve"
     ball = machines.query.distinct(machines.machine_name).all()
     MachineName = machines.query.filter_by(id=machine_id).first()
+
     return render_template(template, title=title, ball=ball, MachineID=MachineName)
 
 
 @Booking_View.route('/process', methods=['POST'])
 def process():
-    Machine_ID = request.form['machine']
-    MachineBookingDate = request.form['date']
-    MachineBookingTime = request.form['time']
-    if Machine_ID and MachineBookingDate and MachineBookingTime:
-        newMachineBookingDate = MachineBookingDate[::-1]
-        newMachineBookingTime = MachineBookingTime[::-1]
-        newMachine_ID = Machine_ID[::-1]
+    Block_ID = request.form['blockId']
+    User_ID = request.form['User_ID']
+    Machine_ID = request.form['Machine_ID']
+    MachineBookingDate = request.form['MachineBookingTime']
+    MachineBookingTime = request.form['MachineBookingDate']
+    DateMachineBooked = request.form['DateMachineBooked']
+    print(Block_ID)
+    print(User_ID)
+   # return 'Block ID:'
 
-        return jsonify({'Machine Booked Date': newMachineBookingDate,
-                        'Machine Booked Time': newMachineBookingTime,
-                        'Machine ID': newMachine_ID})
-
-    return jsonify({'error': 'Missing date'})
+    # if Machine_ID and MachineBookingDate and MachineBookingTime and Block_ID and User_ID and DateMachineBooked:
 
 
-@Booking_View.route('/MachineSchedule/<machine_id>/<string:MachineBlockDateAndTime>')
-def MachineScheduleConfirm(machine_id, MachineBlockDateAndTime):
-    template = "BookingPage/booking.html"
-    title = "Confirm Booking"
-    form = BookingForm()
-    bookings = Student.query.distinct(Users.email).group_by(Users.email).filter(Student.user_id == Users.id).join(
-        Users, Users.id == Student.user_id
-    ).join(
-        machines, machines.id == Student.machine_id
-    ).join(
-        Levels, Levels.id == Student.level_id
-    ).with_entities(
-        Student.id.label("id"),
-        Users.first_name.label("first_name"),
-        Users.last_name.label("last_name"),
-        Users.email.label("email"),
-        machines.machine_name.label("machine_name"),
-        Levels.description.label("level")
-    ).all()
-    return render_template(template, title=title, form=form, booking=bookings)
+# @Booking_View.route('/MachineSchedule/<machine_id>/<string:MachineBlockDateAndTime>')
+# def MachineScheduleConfirm(machine_id, MachineBlockDateAndTime):
+#     template = "BookingPage/booking.html"
+#     title = "Confirm Booking"
+#     form = BookingForm()
+#     bookings = Student.query.distinct(Users.email).group_by(Users.email).filter(Student.user_id == Users.id).join(
+#         Users, Users.id == Student.user_id
+#     ).join(
+#         machines, machines.id == Student.machine_id
+#     ).join(
+#         Levels, Levels.id == Student.level_id
+#     ).with_entities(
+#         Student.id.label("id"),
+#         Users.first_name.label("first_name"),
+#         Users.last_name.label("last_name"),
+#         Users.email.label("email"),
+#         machines.machine_name.label("machine_name"),
+#         Levels.description.label("level")
+#     ).all()
+#     return render_template(template, title=title, form=form, booking=bookings)
 
 
 
---
+++ static/js/form.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $('form').on('submit',function (event) {
+
+        $.ajax({
+            data:{
+                blockId:            $('#blockId').val(),
+                User_ID:            $('#User_ID').val(),
+                Machine_ID:         $('#Machine_ID').val(),
+                MachineBookingTime: $('#MachineBookingTime').val(),
+                MachineBookingDate: $('#MachineBookingDate').val(),
+                DateMachineBooked:  $('#DateMachineBooked').val(),
+          },
+			type : 'POST',
+			url : 'Booking_View.process'
+		});
+		event.preventDefault();
+	});
+});
